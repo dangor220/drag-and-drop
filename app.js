45571dee7a42ac94df24d871b889326941d479a5
dangor220 <dan.gor220.yandex.ru>
2021-09-20 18:10:13 +0300

    Need correct work app

---
+++ app.js
@@ -0,0 +1,40 @@
+const items = document.querySelectorAll('.item');
+const placeholders = document.querySelectorAll('.placeholder');
+
+items.forEach((item) => {
+  item.addEventListener('dragstart', (event) => {
+    setTimeout(() => {
+      event.target.classList.add('hide');
+      event.target.classList.add('active');
+    }, 0);
+    event.target.classList.add('hold');
+  });
+
+  item.addEventListener('dragend', (event) => {
+    event.target.className = 'item';
+  });
+});
+
+for (const placeholder of placeholders) {
+  placeholder.addEventListener('dragover', (event) => {
+    event.preventDefault();
+  });
+  placeholder.addEventListener('dragenter', (event) => {
+    event.target.classList.add('hovered');
+  });
+  placeholder.addEventListener('dragleave', (event) => {
+    event.target.classList.remove('hovered');
+  });
+  placeholder.addEventListener('drop', (event) => {
+    event.target.append(isActive());
+    event.target.classList.remove('hovered');
+  });
+}
+
+function isActive() {
+  for (const item of items) {
+    if (item.classList.contains('active')) {
+      return item
+    } 
+  }
+}
